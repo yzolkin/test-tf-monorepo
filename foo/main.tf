6d6a985344cf710ef13333262a055205f1b350cf
yzolkin <yzolkin@scorewarrior.com>
2024-01-29 15:33:19 +0200

    add module

---
+++ foo/main.tf
@@ -0,0 +1,3 @@
+resource "null_resource" "example" {
+  # Add valid arguments or block definitions here
+}
--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
 module "test" {
-  source = "gitlab.com/scorewarrior/test/google"
-
-  version = "1.0.2"
+  source = "./foo"
 }
 
-
-output "test" {
-  value = module.test.foo
-}
-
-
-module "test_gitlab" {
-  source = "gitlab.com/mattkasa/gitlab-file/local"
-
-  text     = var.test
-  filename = "hello"
-}
-
-
-variables "test" {
+variable "test" {
   type    = string
   default = "Hello, World!"
 }
